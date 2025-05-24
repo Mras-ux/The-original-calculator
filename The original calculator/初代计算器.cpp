34d798ab9ce9d56175f0d596e6ce52b2b9360ec0
Inside <2519638692@qq.com>
2025-05-24 18:46:43 +0800

    Add files via upload

---
+++ The original calculator/初代计算器.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+using namespace std;
+int main()
+{
+    double a,b;
+    cout << "Before you start using it, you must assign the minuend or dividend to a, and the subtrahend or divisor to b. Thank you!";
+    cin >> a >> b;
+    cout << "The sum of these two numbers is:" << a+b << endl;
+    cout << "The difference between these two figures is:" << a-b << endl;
+    cout << "The product of these two numbers is:" << a*b << endl;
+    cout << "The quotient of these two figures:" << a/b << endl;
+    cout << "Thank you for using our calculator, we will continue to optimize it!";
+    return 0;
+}
